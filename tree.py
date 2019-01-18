5c175a6e4ac5599493c905c1c74dcd641920a2e0
Isak Falk <isak.falk@live.se>
2019-01-18 12:40:30 +0000

    Use argparse to enable setting parameters from command line
    The parameters identified are:
    - number of steps to grow tree (tree depth)
    - step size of theta (angular growth)
    - starting radius length
    - the multiplicative update factor of radius

--- tree.py
+++ tree.py
@@ -1,17 +1,7 @@
+import argparse
 from math import cos, sin
 
 from matplotlib import pyplot as plt
-
-radius_length = 1
-radius_multiplicative_update_factor = 0.6
-theta_step_size = 0.1
-num_steps = 5
-
-x_init_start = 0.0
-y_init_start = 0.0
-x_init_end = 0.0
-y_init_end = 1.0
-theta_init = 0.0
 
 
 def grow_branches(leaf, radius_length, theta_step_size):
@@ -44,16 +34,48 @@ def plot_branch(current_leaf, new_leaf_left, new_leaf_right):
              [y, new_y_right])
 
 
-current_leaves = [[x_init_end, y_init_end, theta_init]]
-plt.plot([x_init_start, x_init_end], [y_init_start, y_init_end])
-for steps in range(num_steps):
-    new_leaves = []
-    for current_leaf in current_leaves:
-        new_leaf_left, new_leaf_right = grow_branches(
-            current_leaf, radius_length, theta_step_size)
-        plot_branch(current_leaf, new_leaf_left, new_leaf_right)
-        append_left_right_leaves(new_leaves, new_leaf_left, new_leaf_right)
+def build_tree(num_steps, theta_step_size, radius_length, radius_multiplicative_update_factor,
+               theta_init=0, x_init_start=0.0, y_init_start=0.0, x_init_end=0.0, y_init_end=1.0,
+               plot=True):
+    if plot:
+        plt.plot([x_init_start, x_init_end], [y_init_start, y_init_end])
+    current_leaves = [[x_init_end, y_init_end, theta_init]]
 
-    current_leaves = new_leaves
-    radius_length *= radius_multiplicative_update_factor
-plt.savefig('tree.png')
+    for step in range(num_steps):
+        new_leaves = []
+        for current_leaf in current_leaves:
+            new_leaf_left, new_leaf_right = grow_branches(
+                current_leaf, radius_length, theta_step_size)
+            if plot:
+                plot_branch(current_leaf, new_leaf_left, new_leaf_right)
+            append_left_right_leaves(
+                new_leaves, new_leaf_left, new_leaf_right)
+
+        current_leaves = new_leaves
+        radius_length *= radius_multiplicative_update_factor
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--num_steps", type=int,
+                        help="number of steps to grow the tree for",
+                        default=5)
+    parser.add_argument("-dt", "--theta_step_size",
+                        type=float, help="step size of theta",
+                        default=0.1)
+    parser.add_argument("-r", "--radius_length", type=float,
+                        help="starting radius length",
+                        default=1.0)
+    parser.add_argument("-m", "--radius_multiplicative_update_factor",
+                        type=float, help="factor to multiply radius with at each iteration",
+                        default=0.6)
+    args = parser.parse_args()
+
+    num_steps = args.num_steps
+    theta_step_size = args.theta_step_size
+    radius_length = args.radius_length
+    radius_multiplicative_update_factor = args.radius_multiplicative_update_factor
+
+    build_tree(num_steps, theta_step_size, radius_length,
+               radius_multiplicative_update_factor)
+    plt.savefig('tree.png')
