7043f978912ca1f678b1b71b1bcc4e11d5128080
Isak Falk <isak.falk@live.se>
2019-01-18 11:56:53 +0000

    Make raw numbers into parameters / constants

--- tree.py
+++ tree.py
@@ -3,19 +3,36 @@ from math import cos, sin
 from matplotlib import pyplot as plt
 
 radius_length = 1
-current_leaves = [[0, 1, 0]]
-plt.plot([0, 0], [0, 1])
-for steps in range(5):
+radius_multiplicative_update_factor = 0.6
+theta_step_size = 0.1
+num_steps = 5
+
+x_init_start = 0.0
+y_init_start = 0.0
+x_init_end = 0.0
+y_init_end = 1.0
+theta_init = 0.0
+
+X_INDEX = 0
+Y_INDEX = 1
+THETA_INDEX = 2
+
+LEFT_INDEX = -2
+RIGHT_INDEX = -1
+
+current_leaves = [[x_init_end, y_init_end, theta_init]]
+plt.plot([x_init_start, x_init_end], [y_init_start, y_init_end])
+for steps in range(num_steps):
     new_leaves = []
     for j in range(len(current_leaves)):
-        new_leaves.append([current_leaves[j][0]+radius_length*sin(current_leaves[j][2]-0.1),
-                           current_leaves[j][1]+radius_length*cos(current_leaves[j][2]-0.1), current_leaves[j][2]-0.1])
-        new_leaves.append([current_leaves[j][0]+radius_length*sin(current_leaves[j][2]+0.1),
-                           current_leaves[j][1]+radius_length*cos(current_leaves[j][2]+0.1), current_leaves[j][2]+0.1])
-        plt.plot([current_leaves[j][0], new_leaves[-2][0]],
-                 [current_leaves[j][1], new_leaves[-2][1]])
-        plt.plot([current_leaves[j][0], new_leaves[-1][0]],
-                 [current_leaves[j][1], new_leaves[-1][1]])
+        new_leaves.append([current_leaves[j][X_INDEX]+radius_length*sin(current_leaves[j][THETA_INDEX]-theta_step_size),
+                           current_leaves[j][Y_INDEX]+radius_length*cos(current_leaves[j][THETA_INDEX]-theta_step_size), current_leaves[j][THETA_INDEX]-theta_step_size])
+        new_leaves.append([current_leaves[j][X_INDEX]+radius_length*sin(current_leaves[j][THETA_INDEX]+theta_step_size),
+                           current_leaves[j][Y_INDEX]+radius_length*cos(current_leaves[j][THETA_INDEX]+theta_step_size), current_leaves[j][THETA_INDEX]+theta_step_size])
+        plt.plot([current_leaves[j][X_INDEX], new_leaves[LEFT_INDEX][X_INDEX]],
+                 [current_leaves[j][Y_INDEX], new_leaves[LEFT_INDEX][Y_INDEX]])
+        plt.plot([current_leaves[j][X_INDEX], new_leaves[RIGHT_INDEX][X_INDEX]],
+                 [current_leaves[j][Y_INDEX], new_leaves[RIGHT_INDEX][Y_INDEX]])
     current_leaves = new_leaves
-    radius_length *= 0.6
+    radius_length *= radius_multiplicative_update_factor
 plt.savefig('tree.png')
