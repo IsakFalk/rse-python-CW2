fbeeacbb5dc4496186446015fa28b24ccb212e14
Isak Falk <isak.falk@live.se>
2019-01-18 12:27:26 +0000

    Loop over iterable instead of index of leaves
    Fix

--- tree.py
+++ tree.py
@@ -24,15 +24,15 @@ current_leaves = [[x_init_end, y_init_end, theta_init]]
 plt.plot([x_init_start, x_init_end], [y_init_start, y_init_end])
 for steps in range(num_steps):
     new_leaves = []
-    for j in range(len(current_leaves)):
-        new_leaves.append([current_leaves[j][X_INDEX]+radius_length*sin(current_leaves[j][THETA_INDEX]-theta_step_size),
-                           current_leaves[j][Y_INDEX]+radius_length*cos(current_leaves[j][THETA_INDEX]-theta_step_size), current_leaves[j][THETA_INDEX]-theta_step_size])
-        new_leaves.append([current_leaves[j][X_INDEX]+radius_length*sin(current_leaves[j][THETA_INDEX]+theta_step_size),
-                           current_leaves[j][Y_INDEX]+radius_length*cos(current_leaves[j][THETA_INDEX]+theta_step_size), current_leaves[j][THETA_INDEX]+theta_step_size])
-        plt.plot([current_leaves[j][X_INDEX], new_leaves[LEFT_INDEX][X_INDEX]],
-                 [current_leaves[j][Y_INDEX], new_leaves[LEFT_INDEX][Y_INDEX]])
-        plt.plot([current_leaves[j][X_INDEX], new_leaves[RIGHT_INDEX][X_INDEX]],
-                 [current_leaves[j][Y_INDEX], new_leaves[RIGHT_INDEX][Y_INDEX]])
+    for current_leaf in current_leaves:
+        new_leaves.append([current_leaf[X_INDEX]+radius_length*sin(current_leaf[THETA_INDEX]-theta_step_size),
+                           current_leaf[Y_INDEX]+radius_length*cos(current_leaf[THETA_INDEX]-theta_step_size), current_leaf[THETA_INDEX]-theta_step_size])
+        new_leaves.append([current_leaf[X_INDEX]+radius_length*sin(current_leaf[THETA_INDEX]+theta_step_size),
+                           current_leaf[Y_INDEX]+radius_length*cos(current_leaf[THETA_INDEX]+theta_step_size), current_leaf[THETA_INDEX]+theta_step_size])
+        plt.plot([current_leaf[X_INDEX], new_leaves[LEFT_INDEX][X_INDEX]],
+                 [current_leaf[Y_INDEX], new_leaves[LEFT_INDEX][Y_INDEX]])
+        plt.plot([current_leaf[X_INDEX], new_leaves[RIGHT_INDEX][X_INDEX]],
+                 [current_leaf[Y_INDEX], new_leaves[RIGHT_INDEX][Y_INDEX]])
     current_leaves = new_leaves
     radius_length *= radius_multiplicative_update_factor
 plt.savefig('tree.png')
